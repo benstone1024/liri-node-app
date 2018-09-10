0ba25c0faea644f826f1ffc793cfef381cd9b591
benjamin j stone  jr <RadioAktivemusic@benjamins-imac.attlocal.net>
2018-09-10 14:32:41 -0400

    update

--- key.js
+++ key.js
@@ -1,6 +1,8 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: 'c9ab5a45b948408f8b4d7e7a97caac6b',
+  secret:'c4db3b9d09234d8b9318484d5db8386f',
 };
+
+var spotify = new Spotify(keys.spotify);
--- liri.js
+++ liri.js
@@ -1,1 +1,6 @@
+require("dotenv").config();
+
+
 var apiKeys = require("./keys.js")
+
+var spotify = require('spotify')
